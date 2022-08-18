26a8eef2593b2350647f8d7ffae759a32f5e65c1
Daniil Gorin <daniil009.gorin@yandex.ru>
2022-08-18 22:40:55 +0300

    Добавьте файлы проекта.

---
+++ Task 57 Unification/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_57_Unification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firtstSoldiers = new List<string>(){"Антон", "Борис", "Макс", "Артём", "Богдан"};
+            List<string> secondSoldiers = new List<string>() { "Марк", "Лиза", "Джексон"};
+
+            var soldiers = secondSoldiers.Union(firtstSoldiers.Where(soldier => soldier.StartsWith("Б")));
+
+            foreach (var soldier in soldiers)
+            {
+                Console.WriteLine(soldier);
+            }
+        }
+    }
+}
